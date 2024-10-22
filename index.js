e682ae6d3ad7faa53ec81990007e24efa0897bbe
Emanuele Liardo <emanuele.liardo@codiceplastico.com>
2024-10-22 23:30:41 +0200

    fix lint (#143)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { mapResponse } = require('./functions');
-const getHistoricalData = require('./src/getHistoricalData')
+const getHistoricalData = require('./src/getHistoricalData');
 
 const validPeriod = ['P1D', 'P1W', 'P1M', 'P3M', 'P6M', 'P1Y', 'P5Y', 'MAX'];
 const validInterval = ['PT1M', 'PT5M', 'PT15M', 'PT30M', 'PT1H', 'PT5H', 'P1D', 'P1W', 'P1M'];
@@ -90,5 +90,5 @@ async function investing(pairId, period = 'P1M', interval = 'P1D', pointscount =
 
 module.exports = {
   investing,
-  getHistoricalData
-}
+  getHistoricalData,
+};

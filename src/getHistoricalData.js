b09888342b428925b715b181ce20560a4725959d
Emanuele Liardo <emanuele.liardo@codiceplastico.com>
2024-12-01 04:59:48 +0100

    test

--- src/getHistoricalData.js
+++ src/getHistoricalData.js
@@ -5,18 +5,14 @@ const buildUrl = ({ input, resolution = 'D', from, to } = {}) => {
   const query = new URLSearchParams({
     symbol: getPairId(input),
     resolution,
-    from: from && from.getTime() / 1000,
-    to: to && to.getTime() / 1000,
+    from: from && Math.round(from.getTime() / 1000),
+    to: to && Math.round(to.getTime() / 1000),
   });
   return 'https://tvc6.investing.com/d8f62270e64f9eb6e4e6a07c3ffeab0b/1729428526/9/9/16/history?' + query;
 };
 
 const getHistoricalData = async (params) => {
-  const response = await fetch(buildUrl(params), {
-    headers: new Headers({
-      'upgrade-insecure-requests': '1',
-    }),
-  });
+  const response = await fetch(buildUrl(params));
 
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
